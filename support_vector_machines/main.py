9e4f7f8a502865f2722e8729ff47ad7c918822ea
Martin Pettersson <kontakt@martinpettersson.se>
2015-10-07 21:09:24 +0200

    Added kernel function and datapoint generator

--- support_vector_machines/main.py
+++ support_vector_machines/main.py
@@ -1,4 +1,23 @@
 __author__ = 'martinpettersson'
 from cvxopt.solvers import qp
 from cvxopt.base import matrix
-import numpy, pylab, random, math
+import pylab, random, math
+import numpy as np
+
+def my_kernel(x,y):
+    return np.dot(x, y.T)
+
+def generate_datapoints():
+    classA = [(random.normalvariate(-1.5, 1),
+               random.normalvariate(0.5, 1), 1.0)
+              for i in range(5)] + \
+                [(random.normalvariate(1.5,1),
+                  random.normalvariate(0.5,1), 1.0)
+                 for i in range(5)]
+    classB = [(random.normalvariate(0.0,0.5),
+                  random.normalvariate(-0.5,0.5), -1.0)
+                 for i in range(10)]
+    data = classA + classB
+    random.shuffle(data)
+
+generate_datapoints()

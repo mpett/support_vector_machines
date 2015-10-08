7d59c97b4e4d638e1c0f90d1627202e484e72dee
Martin Pettersson <kontakt@martinpettersson.se>
2015-10-08 22:33:01 +0200

    P Matrix generator

--- support_vector_machines/main.py
+++ support_vector_machines/main.py
@@ -5,17 +5,34 @@ import pylab, random, math
 import numpy as np
 
 def linear_kernel(x,y):
-    return np.dot(x, y.T) + 1
+    return np.dot(x, np.transpose(y)) + 1
 
 def polynomial_kernel(x,y,p):
-    return math.pow((np.dot(x, y.T) + 1), p)
+    return math.pow((np.dot(x, np.transpose(y)) + 1), p)
 
 def radial_basis_function_kernel(x,y,sigma):
     exponent = math.pow(x-y,2) / math.pow(2*sigma,2)
     return math.exp(-exponent)
 
 def sigmoid_kernel(x,y,k,delta):
-    return math.tanh(k*np.dot(x.T,y) - delta)
+    return math.tanh(k*np.dot(np.transpose(x),y) - delta)
+
+def indicator(x,y):
+    return []
+
+def generate_p_matrix(data):
+    p_matrix = [[0 for element in range(len(data))] for element in range(len(data))]
+    t_values = [0 for element in range(len(data))]
+    vectors = [0 for element in range(len(data))]
+    index = 0
+    for element in data:
+        t_values[index] = element[2]
+        vectors[index] = element[:2]
+        index+=1
+    for i in range(len(data)):
+        for j in range(len(data)):
+            p_matrix[i][j] = t_values[i]*t_values[j]*linear_kernel(vectors[i],vectors[j])
+    return p_matrix
 
 def generate_datapoints():
     classA = [(random.normalvariate(-1.5, 1),
@@ -29,7 +46,8 @@ def generate_datapoints():
                  for i in range(10)]
     data = classA + classB
     random.shuffle(data)
-    plot_datapoints(classA,classB)
+    #plot_datapoints(classA,classB)
+    return data
 
 def plot_datapoints(classA, classB):
     pylab.hold(True)
@@ -41,4 +59,17 @@ def plot_datapoints(classA, classB):
                'ro')
     pylab.show()
 
-generate_datapoints()
+def plot_decision_boundary():
+    xrange = np.arange(-4,4,0.05)
+    yrange = np.arange(-4,4,0.05)
+    grid = matrix([[indicator(x,y) for y in yrange] for x in xrange])
+    pylab.contour(xrange,yrange,grid,
+                  (-1.0,0.0,1.0),
+                  colors=('red','black','blue'),
+                  linewidths=(1,3,1))
+
+def main():
+    data = generate_datapoints()
+    N = len(data)
+
+main()

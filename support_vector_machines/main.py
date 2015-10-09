27b62dc9205b4ed889370024060142a9d5e73dcf
Martin Pettersson <kontakt@martinpettersson.se>
2015-10-09 11:19:27 +0200

    Indicator function

--- support_vector_machines/main.py
+++ support_vector_machines/main.py
@@ -4,8 +4,12 @@ from cvxopt.base import matrix
 import pylab, random, math
 import numpy as np
 
+alpha = []
+non_zero_alphas = []
+data = []
+
 def linear_kernel(x,y):
-    return np.dot(x, np.transpose(y)) + 1
+    return np.dot(x, np.transpose(y))
 
 def polynomial_kernel(x,y,p):
     return math.pow((np.dot(x, np.transpose(y)) + 1), p)
@@ -18,7 +22,13 @@ def sigmoid_kernel(x,y,k,delta):
     return math.tanh(k*np.dot(np.transpose(x),y) - delta)
 
 def indicator(x,y):
-    return []
+    x_star = [x,y]
+    sum = 0
+    t_values = [0 for element in range(len(data))]
+    vectors = [0 for element in range(len(data))]
+    for index in non_zero_alphas:
+        sum += alpha[index]*t_values[index]*linear_kernel(x_star,vectors[index])
+    return sum
 
 def generate_p_matrix(data):
     p_matrix = [[0 for element in range(len(data))] for element in range(len(data))]
@@ -46,7 +56,7 @@ def generate_datapoints():
                  for i in range(10)]
     data = classA + classB
     random.shuffle(data)
-    #plot_datapoints(classA,classB)
+    plot_datapoints(classA,classB)
     return data
 
 def plot_datapoints(classA, classB):
@@ -57,9 +67,6 @@ def plot_datapoints(classA, classB):
     pylab.plot([p[0] for p in classB],
                [p[1] for p in classB],
                'ro')
-    pylab.show()
-
-def plot_decision_boundary():
     xrange = np.arange(-4,4,0.05)
     yrange = np.arange(-4,4,0.05)
     grid = matrix([[indicator(x,y) for y in yrange] for x in xrange])
@@ -67,20 +74,30 @@ def plot_decision_boundary():
                   (-1.0,0.0,1.0),
                   colors=('red','black','blue'),
                   linewidths=(1,3,1))
+    pylab.show()
 
 def main():
+    global alpha
+    global non_zero_alphas
+    global data
     data = generate_datapoints()
     N = len(data)
     q_vector = [-1.0 for e in range(N)]
     h_vector = [0.0] * N
     g_matrix = [[0.0 for element in range(N)] for element in range(N)]
     for i in range(N):
-        g_matrix[i][i] = -1
+        g_matrix[i][i] = -1.0
     p_matrix = generate_p_matrix(data)
     P = matrix(p_matrix)
     q = matrix(q_vector)
     G = matrix(g_matrix)
     h = matrix(h_vector)
-    qp(P,q,G,h)
-    
+    r=qp(P,q,G,h)
+    alpha = list(r['x'])
+    non_zero_alphas = [i for i, e in enumerate(alpha) if e > 0.00005]
+    print(alpha)
+    print(non_zero_alphas)
+
+
 main()
+
